f27e7416e699a0de1dd637d3dc0bb7e9f8e00a97
Victor Alexiev <vic.alexiev@gmail.com>
2013-11-15 21:12:09 +0200

    jtwitter added

--- src/telerik/academy/agora/StatusActivity.java
+++ src/telerik/academy/agora/StatusActivity.java
@@ -1,22 +1,41 @@
 package telerik.academy.agora;
 
-import android.os.Bundle;
+import winterwell.jtwitter.OAuthSignpostClient;
+import winterwell.jtwitter.Twitter;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class StatusActivity extends Activity {
+public class StatusActivity extends Activity implements OnClickListener { //
 
+	private static final String TAG = "StatusActivity";
+	EditText editText;
+	Button updateButton;
+	Twitter twitter;
+
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.status);
+		
+		// Find views
+		editText = (EditText) findViewById(R.id.editText);
+		updateButton = (Button) findViewById(R.id.buttonUpdate);
+
+		updateButton.setOnClickListener(this);
+
+		twitter = new Twitter("student", "password");
+		twitter.setAPIRootUrl("http://agora.academy.telerik/api");
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.status, menu);
-		return true;
+	// Called when button is clicked
+	public void onClick(View view) {
+		twitter.setStatus(editText.getText().toString());
+		Log.d(TAG, "onClicked");
 	}
-
 }
