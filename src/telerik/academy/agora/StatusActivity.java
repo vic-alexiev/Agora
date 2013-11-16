822491118f67cca234302a24425040207b6add5b
Victor Alexiev <vic.alexiev@gmail.com>
2013-11-16 14:35:19 +0200

    Characters counter added.

--- src/telerik/academy/agora/StatusActivity.java
+++ src/telerik/academy/agora/StatusActivity.java
@@ -1,14 +1,16 @@
 package telerik.academy.agora;
 
-import winterwell.jtwitter.OAuthSignpostClient;
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class StatusActivity extends Activity implements OnClickListener { //
 
@@ -22,7 +24,7 @@ public class StatusActivity extends Activity implements OnClickListener { //
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.status);
-		
+
 		// Find views
 		editText = (EditText) findViewById(R.id.editText);
 		updateButton = (Button) findViewById(R.id.buttonUpdate);
@@ -33,9 +35,41 @@ public class StatusActivity extends Activity implements OnClickListener { //
 		twitter.setAPIRootUrl("http://agora.academy.telerik/api");
 	}
 
+	// Asynchronously posts to twitter
+	private class PostToTwitter extends AsyncTask<String, Integer, String> {
+
+		// Called to initiate the background activity
+		@Override
+		protected String doInBackground(String... statuses) {
+			try {
+				winterwell.jtwitter.Status status = twitter.updateStatus(statuses[0]);
+				return status.text;
+			} catch (TwitterException e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+
+		// Called when there's a status to be updated
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+			// Not used in this case
+		}
+
+		// Called once the background activity has completed
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG)
+					.show();
+		}
+	}
+
 	// Called when button is clicked
 	public void onClick(View view) {
-		twitter.setStatus(editText.getText().toString());
+		String status = editText.getText().toString();
+		new PostToTwitter().execute(status);
 		Log.d(TAG, "onClicked");
 	}
 }
